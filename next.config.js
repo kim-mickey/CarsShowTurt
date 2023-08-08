65ae64782d8dcb9431206cb12b09b17190864764
kim-mickey <evansngila01@gmail.com>
2023-08-08 15:46:55 +0300

    updated nextjs config file

--- next.config.js
+++ next.config.js
@@ -3,10 +3,8 @@ const nextConfig = {
     images: {
         domains: ['cdn.imagin.studio']
     },
-    {
-        typescript: {
-            ignoreBuildErrors: true,
-        }
+    typescript: {
+        ignoreBuildErrors: true,
     }
 }
 
